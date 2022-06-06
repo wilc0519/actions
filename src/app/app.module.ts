5a2b336aa72b4faad2d8a68ffa048398ecde8d2e
wilc0519 <wilc0519@gmail.com>
2022-06-06 17:39:48 -0500

    actions hellow word

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetComponent
   ],
   imports: [
     BrowserModule,
